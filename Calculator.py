c2dd5ca9c16f650e4ca19dcb4a887d5fc16873f8
_Mays_Studio_ Official <ivanlvicyn@gmail.com>
2024-09-15 18:43:46 +0300

    Create Calculator.py
    Калькулятор который я сделал

---
+++ Calculator.py
@@ -0,0 +1,42 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "Ошибка! Деление на ноль."
+    return x / y
+
+print("Выберите операцию:")
+print("1. Сложение")
+print("2. Вычитание")
+print("3. Умножение")
+print("4. Деление")
+
+while True:
+    choice = input("Введите номер операции (1/2/3/4): ")
+
+    if choice in ('1', '2', '3', '4'):
+        num1 = float(input("Введите первое число: "))
+        num2 = float(input("Введите второе число: "))
+
+        if choice == '1':
+            print(f"{num1} + {num2} = {add(num1, num2)}")
+        elif choice == '2':
+            print(f"{num1} - {num2} = {subtract(num1, num2)}")
+        elif choice == '3':
+            print(f"{num1} * {num2} = {multiply(num1, num2)}")
+        elif choice == '4':
+            print(f"{num1} / {num2} = {divide(num1, num2)}")
+    else:
+        print("Неверный ввод")
+
+    next_calculation = input("Хотите выполнить еще одно вычисление? (да/нет): ")
+    if next_calculation.lower() != 'да':
+        break
+        
